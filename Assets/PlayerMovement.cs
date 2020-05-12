379960d9ba3811546d8701dea977a87d5473738c
Meghna Raswan <raswan@chapman.edu>
2020-05-11 19:09:10 -0700

    initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    public float runSpeed = 40f;
+
+    private float horizontalMove = 0f;
+    private bool jumpFlag = false;
+    private bool jump = false;
+    private bool crouch = false;
+
+    public int cherries = 0;
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (jumpFlag)
+        {
+            animator.SetBool("IsJumping", true);
+            jumpFlag = false;
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Collectible")
+        {
+            Destroy(collision.gameObject);
+            cherries += 1;
+        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+    public void OnCrouching(bool isCrouching)
+    {
+        animator.SetBool("IsCrouching", isCrouching);
+    }
+
+    private void FixedUpdate()
+    {
+        // move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+
+        if (jump)
+        {
+            jumpFlag = true;
+            jump = false;
+        }
+    }
+
+}
